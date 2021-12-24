711d1cbbc3f6d102c0fc0f5b92407ebb61dad994
AnthonyGunardi <anthonygunardi@gmail.com>
2021-12-24 09:40:02 +0700

    new

--- application/views/scan/scan_desktop.php
+++ application/views/scan/scan_desktop.php
@@ -21,8 +21,11 @@
                         <label for="sourceSelect">Change video source:</label>
                         <select id="sourceSelect" style="max-width:400px"></select>
                     </div>
-                    <div>
-                        <video id="video" width="800" height="600" style="border: 1px solid gray"></video>
+                    <div style="display:flex">
+                        <video id="video" width="512" height="384" style="border: 1px solid gray"></video>
+                        <div id="timer" style="margin:auto; font-size:150px; font-weight: bold; display:none">
+                        00:03
+                        </div>
                     </div>
                     <ul class="list-unstyled" style="margin-top:9px">
                         <li style="font-size: 150%"><b>Untuk melakukan presensi, dekatkan QR Code ke kamera sampai terdengar bunyi notifikasi pertama.</b></li>
@@ -39,78 +42,110 @@
 
 <script type="text/javascript" src="<?php echo base_url()?>assets/plugins/zxing/zxing.min.js"></script>
 <script type="text/javascript">
-window.addEventListener('load', function () {
-    let selectedDeviceId;
-    let audio = new Audio("assets/audio/beep.mp3");
-    let audio2 = new Audio("assets/audio/beep-2.mp3");
-    const codeReader = new ZXing.BrowserQRCodeReader()
-    console.log('ZXing code reader initialized')
-    codeReader.getVideoInputDevices()
-    .then((videoInputDevices) => {
-        const sourceSelect = document.getElementById('sourceSelect')
-        selectedDeviceId = videoInputDevices[0].deviceId
-        if (videoInputDevices.length >= 1) {
-            videoInputDevices.forEach((element) => {
-                const sourceOption = document.createElement('option')
-                sourceOption.text = element.label
-                sourceOption.value = element.deviceId
-                sourceSelect.appendChild(sourceOption)
-            })
-            sourceSelect.onchange = () => {
-                selectedDeviceId = sourceSelect.value;
-            };
-            const sourceSelectPanel = document.getElementById('sourceSelectPanel')
-            sourceSelectPanel.style.display = 'block'
+    let timer = document.getElementById("timer");
+    let startingTime = 2
+    let startTimer;
+    let startStopTimer = () => {
+        if(startTimer){
+            clearInterval(startTimer);
+            timer.innerHTML = '00:03'
+            startingTime = 2
+            return startTimer = undefined
+        }else{
+            return startTimer = setInterval(()=>{
+            timer.innerHTML = `00:0${startingTime--}`
+            }, 1000)
         }
-        codeReader.decodeFromInputVideoDevice(selectedDeviceId, 'video').then((result) => {
-            console.log(result)
-            document.getElementById('result').textContent = result.text
-            if(result != null){
-                audio2.play();
+    }
+
+    function showHideTimer() {
+        let x = document.getElementById("timer");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+</script>
+<script type="text/javascript">
+    window.addEventListener('load', function () {
+        let selectedDeviceId;
+        let audio = new Audio("assets/audio/beep.mp3");
+        let audio2 = new Audio("assets/audio/beep-2.mp3");
+        const codeReader = new ZXing.BrowserQRCodeReader();
+
+        console.log('ZXing code reader initialized')
+        codeReader.getVideoInputDevices()
+        .then((videoInputDevices) => {
+            const sourceSelect = document.getElementById('sourceSelect')
+            selectedDeviceId = videoInputDevices[0].deviceId
+            if (videoInputDevices.length >= 1) {
+                videoInputDevices.forEach((element) => {
+                    const sourceOption = document.createElement('option')
+                    sourceOption.text = element.label
+                    sourceOption.value = element.deviceId
+                    sourceSelect.appendChild(sourceOption)
+                })
+                sourceSelect.onchange = () => {
+                    selectedDeviceId = sourceSelect.value;
+                };
+                const sourceSelectPanel = document.getElementById('sourceSelectPanel')
+                sourceSelectPanel.style.display = 'block'
             }
-            const video = document.getElementById("video");
-            const canvas = document.createElement("canvas");
-            // scale the canvas accordingly
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-            // draw the video at that frame
-            setTimeout(function() {
-                canvas.getContext('2d')
-                .drawImage(video, 0, 0, canvas.width, canvas.height);
+            codeReader.decodeFromInputVideoDevice(selectedDeviceId, 'video').then((result) => {
+                console.log(result)
+                document.getElementById('result').textContent = result.text
+                if(result != null){
+                    audio2.play();
+                    showHideTimer();
+                    startStopTimer();
+                }
+                const video = document.getElementById("video");
+                const canvas = document.createElement("canvas");
+                // scale the canvas accordingly
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                // draw the video at that frame
+                setTimeout(function() {
+                    canvas.getContext('2d')
+                    .drawImage(video, 0, 0, canvas.width, canvas.height);
 
-                var formData = new FormData(),
-                uploadedImageName = result.text+'.png';
+                    var formData = new FormData(),
+                    uploadedImageName = result.text+'.png';
 
-                canvas.toBlob(function (blob) {
-                    formData.append('gambar', blob, uploadedImageName);
-                    formData.append('id_karyawan', result.text);
+                    canvas.toBlob(function (blob) {
+                        formData.append('gambar', blob, uploadedImageName);
+                        formData.append('id_karyawan', result.text);
 
-                    // console.log(formData);
-                    $.ajax({
-                        data: formData,
-                        type: "POST",
-                        url: '<?=base_url("scan/cek_id")?>',
-                        processData: false,
-                        contentType: false,
-                        success: function(response){
-                            //if request if made successfully then the response represent the data
-                            audio.play();
-                            window.location.reload();
-                        }
+                        // console.log(formData);
+                        $.ajax({
+                            data: formData,
+                            type: "POST",
+                            url: '<?=base_url("scan/cek_id")?>',
+                            processData: false,
+                            contentType: false,
+                            success: function(response){
+                                //if request if made successfully then the response represent the data
+                                audio.play();
+                                // showHideTimer();
+                                startStopTimer();
+                                window.location.reload();
+                            }
+                        });
                     });
-                });
-            }, 3000);
+                }, 3000);
 
-           
-            // $('#button').submit();
-        }).catch((err) => {
-            console.error(err)
-            document.getElementById('result').textContent = err
+            
+                // $('#button').submit();
+            }).catch((err) => {
+                console.error(err)
+                document.getElementById('result').textContent = err
+            })
+            console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
         })
-        console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
+        .catch((err) => {
+            console.error(err)
+        })
     })
-    .catch((err) => {
-        console.error(err)
-    })
-})
 </script>
--- database/db_absensi.sql
+++ database/db_absensi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2021 at 09:10 AM
+-- Generation Time: Dec 22, 2021 at 09:56 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
@@ -77,7 +77,8 @@ CREATE TABLE `jabatan` (
 
 INSERT INTO `jabatan` (`id_jabatan`, `nama_jabatan`) VALUES
 (1, 'WEB DEVELOPMENT'),
-(2, 'BROADCASTING');
+(2, 'MEDIA'),
+(3, 'BUSINESS RELATION');
 
 -- --------------------------------------------------------
 
@@ -99,7 +100,12 @@ CREATE TABLE `karyawan` (
 --
 
 INSERT INTO `karyawan` (`id`, `id_karyawan`, `nama_karyawan`, `jabatan`, `id_shift`, `gedung_id`) VALUES
-(1, 'W2112170', 'Anthony Gunardi Purwadipura', 1, 1, 1);
+(1, 'W2112170', 'Anthony Gunardi Purwadipura', 1, 1, 1),
+(4, 'M2112001', 'Tester 01', 2, 1, 1),
+(5, 'B2112004', 'Tester 02', 3, 1, 1),
+(6, 'W2112005', 'Tester 03', 1, 1, 1),
+(7, 'B2112006', 'Tester 04', 3, 1, 1),
+(8, 'M2112007', 'Tester 05', 2, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -199,7 +205,8 @@ INSERT INTO `presensi` (`id_absen`, `id_karyawan`, `tgl`, `jam_msk`, `jam_klr`, 
 (1, 'W2112170', '2021-12-11', '09:13:30', '04:38:20', 1, '', '', 2),
 (3, 'W2112170', '2021-12-13', '02:39:15', '04:09:09', 1, '', 'http://localhost/sistem-absensi/uploads/cam_image/W21121701.png', 2),
 (4, 'W2112170', '2021-12-14', '09:38:00', '17:12:00', 1, '', 'http://localhost/sistem-absensi/uploads/cam_image/W2112170.png', 2),
-(31, 'W2112170', '2021-12-20', '10:33:09', '00:00:00', 6, '', 'http://localhost/sistem-absensi/uploads/cam_image/W211217030.png', 1);
+(33, 'B2112001', '2021-12-21', '14:29:20', '14:34:10', 6, '', 'http://localhost/sistem-absensi/uploads/cam_image/B2112001.png', 2),
+(35, 'W2112170', '2021-12-21', '00:00:00', '00:00:00', 4, '', '', 3);
 
 -- --------------------------------------------------------
 
@@ -273,7 +280,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `email`, `activation_selector`, `activation_code`, `forgotten_password_selector`, `forgotten_password_code`, `forgotten_password_time`, `remember_selector`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
-(1, '::1', 'Admin', '$2y$12$MPcQlOck9fzd/5UjJ6iIXuhZivhkXdfoVD2xFXpZTnZ2IWQw/nFhW', 'admin@admin.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1556798313, 1639968196, 1, 'Admin', 'MIC Transformer', NULL, '123412341234'),
+(1, '::1', 'Admin', '$2y$12$MPcQlOck9fzd/5UjJ6iIXuhZivhkXdfoVD2xFXpZTnZ2IWQw/nFhW', 'admin@admin.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1556798313, 1640142332, 1, 'Admin', 'MIC Transformer', NULL, '123412341234'),
 (2, '::1', 'Operator', '$2y$10$ibxBt.RYG7jib0JxBxB1.usc9pzjqUqPPTvwLgPp1gMRKg6VMLLei', 'operator@operator.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1562178403, 1639639442, 1, 'operator', '2', NULL, '34890234809238490'),
 (3, '::1', 'Staff', '$2y$12$RHPXgho6UJLQWv8W3F9oJekbSnFYXyhNADZvuqaYmGEoUD6Yl1Hd.', 'staff@staff.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1564294648, 1564294659, 1, 'staf', 'staff', NULL, '239423849234890');
 
@@ -400,13 +407,13 @@ ALTER TABLE `groups`
 -- AUTO_INCREMENT for table `jabatan`
 --
 ALTER TABLE `jabatan`
-  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `kehadiran`
@@ -430,7 +437,7 @@ ALTER TABLE `menu`
 -- AUTO_INCREMENT for table `presensi`
 --
 ALTER TABLE `presensi`
-  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `shift`
